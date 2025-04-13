dfaaa11b674896b36509a12f833d0fb524b14372
AXCWG <xiey0@qq.com>
2025-04-13 21:06:10 +0800

    Unresolved

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,86 @@
+import 'package:cookie_store/cookie_store.dart';
 import 'package:flutter/material.dart';
+import 'package:http/browser_client.dart';
 import 'package:http/http.dart' as http;
 
-void main() {
+class Singletons {
+  static CookieStore cookieStore = CookieStore();
+  static var client = BrowserClient()..withCredentials = true;
+
+  static Future<int> initCookie() async {
+    // var req = http.Request("POST", Uri.parse("http://localhost:5099/api/User/Login"));
+    // req.body = "{\"username\": \"xie\",\"password\": \"moyingren2015\"}";
+    // req.headers["Content-Type"] = "application/json";
+    client.get(Uri.parse("http://localhost:5099/api/User/Logout"));
+    var res = await client.post(
+      Uri.parse("http://localhost:5099/api/User/Login"),
+      body: "{\"username\": \"xie\",\"password\": \"moyingren2015\"}",
+      headers: {"Content-Type": "application/json"},
+    );
+    var resUserApi = await client.get(
+      Uri.parse("http://localhost:5099/api/User/UserApi"),
+    );
+    print(resUserApi.body);
+    // print((await http.get(Uri.parse("http://localhost:5099/api/User/UserApi") )).body);
+    // cookieStore.updateCookies(res.headers["Set-Cookie"], "localhost:5099", "/");
+
+    return 0;
+  }
+
+  static Future<Widget> getUserProfile(var uuid) async {
+    var res = await client.get(
+      Uri.parse(
+        "http://localhost:5099/api/User/ProfileGet?uuidDestination=" + uuid,
+      ),
+    );
+    return Image(image: MemoryImage(res.bodyBytes));
+  }
+}
+
+void main() async {
+  await Singletons.initCookie();
   runApp(const MainApp());
+}
+
+class Homepage extends StatefulWidget {
+  const Homepage();
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Card(
+          child: ListTile(
+            leading: FutureBuilder(
+              future: Singletons.getUserProfile(
+                "1d27c50b-3492-4627-b886-2fa615e47f8e",
+              ),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  if (snapshot.hasError) {
+                    return Icon(Icons.error);
+                  } else {
+                    return snapshot.data!;
+                  }
+                } else {
+                  return CircularProgressIndicator();
+                }
+              },
+            ),
+            title: Text("Contact 1"),
+            subtitle: Text("Stuffs"),
+          ),
+        ),
+        Card(
+          child: ListTile(title: Text("Contact 2"), subtitle: Text("Stuffs")),
+        ),
+      ],
+    );
+  }
 }
 
 class MainApp extends StatefulWidget {
@@ -11,9 +89,7 @@ class MainApp extends StatefulWidget {
   @override
   State<MainApp> createState() => _MainAppState();
 }
-Future<http.Response> fetchAvatar(var uuid){
-  return http.get(Uri.parse("http://localhost:5099/api/User/ProfileGet?uuid="+uuid)); 
-}
+
 class _MainAppState extends State<MainApp> {
   int currentIndex = 0;
   @override
@@ -47,26 +123,7 @@ class _MainAppState extends State<MainApp> {
             ),
           ],
         ),
-        body:
-            [
-              Column(
-                children: [
-                  Card(
-                    child: ListTile(
-                      title: Text("Contact 1"),
-                      subtitle: Text("Stuffs"),
-                    ),
-                  ),
-                  Card(
-                    child: ListTile(
-                      title: Text("Contact 2"),
-                      subtitle: Text("Stuffs"),
-                    ),
-                  ),
-                ],
-              ),
-              const Center(child: Text("Settings")),
-            ][currentIndex],
+        body: IndexedStack(children: [Homepage(), const Center(child: Text("Settings"))], index: currentIndex,),
       ),
     );
   }
